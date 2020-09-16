d74f5b215fbe12dc753098898b54f07f88e42fb9
vivek22117 <vivekmishra22117@gmail.com>
2020-09-17 00:14:21 +0530

    Adding CheckPalindrome Algo

---
+++ String-Algo/src/main/java/com/ds/algo/examples/CheckPalindrome.java
@@ -0,0 +1,34 @@
+package com.ds.algo.examples;
+
+public class CheckPalindrome {
+
+    private static final String input = "Malayalam";
+
+    public static void main(String[] args) {
+        CheckPalindrome checkPalindrome = new CheckPalindrome();
+        boolean palindrome = checkPalindrome.isPalindrome(input);
+
+        System.out.println("Is Palindrome: " + palindrome);
+    }
+
+    private boolean isPalindrome(String input) {
+        if(input.length() == 0) {
+            return false;
+        }
+        input = input.toLowerCase();
+        int forwardIndx = 0;
+        int backwardIndx = input.length() -1;
+
+        while (forwardIndx < backwardIndx) {
+            char forwardChar = input.charAt(forwardIndx);
+            char backwardChar = input.charAt(backwardIndx);
+
+            if(forwardChar != backwardChar) {
+                return false;
+            }
+            forwardIndx++;
+            backwardIndx--;
+        }
+        return true;
+    }
+}
---
+++ String-Algo/src/main/java/com/ds/algo/examples/CheckPalindromeInPermutation.java
@@ -0,0 +1,4 @@
+package com.ds.algo.examples;
+
+public class CheckPalindromeInPermutation {
+}
