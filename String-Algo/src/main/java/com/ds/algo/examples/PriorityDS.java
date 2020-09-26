26b69a05933bc705837bb25a4f7b0aa863903976
vivek22117 <vivekmishra22117@gmail.com>
2020-09-26 11:55:21 +0530

    Updating Priority Queue DS

--- String-Algo/src/main/java/com/ds/algo/examples/PriorityDS.java
+++ String-Algo/src/main/java/com/ds/algo/examples/PriorityDS.java
@@ -1,13 +1,77 @@
 package com.ds.algo.examples;
 
-import java.util.Stack;
+import java.lang.reflect.Array;
 
-public class PriorityDS {
+public class PriorityDS<T> {
 
-    private static Stack<Employee> highPriorityElements = new Stack<>();
-    private static Stack<Employee> elements = new Stack<>();
+    private Integer MAX_SIZE = 10;
+    private static Integer SIZE = 0;
 
-    public static class Employee {
+    private T[] elements;
+
+    public PriorityDS(Class<T> clazz) {
+        this.elements = (T[]) Array.newInstance(clazz, MAX_SIZE);
+    }
+
+    public boolean isEmpty() {
+        return SIZE == 0;
+    }
+
+    public void putElement(T element){
+        if(SIZE == elements.length) {
+            return;
+        }
+        elements[SIZE] = element;
+        rearrangeElements(SIZE);
+        SIZE++;
+    }
+
+    private void rearrangeElements(Integer index) {
+       int nextIndx = index + 1;
+
+        if(nextIndx != SIZE - 1) {
+            if(compareElements(elements[index], elements[nextIndx]) < 0){
+                swap(nextIndx, index);
+                rearrangeElements(nextIndx);
+            }
+        }
+    }
+
+    private void shiftElements(Integer index) {
+        int previousIndx = index - 1;
+
+        if(previousIndx != -1) {
+            if(compareElements(elements[index], elements[previousIndx]) > 0) {
+                swap(previousIndx, index);
+                shiftElements(previousIndx);
+            }
+        }
+    }
+
+    private void swap(int index, Integer previousIndx) {
+        T element = elements[previousIndx];
+        elements[previousIndx] = elements[index];
+        elements[index] = element;
+    }
+
+    private int compareElements(T element1, T element2) {
+
+        return -1;
+    }
+
+    public T getElement() {
+        if(isEmpty()) {
+            return null;
+        }
+        T priorityElement =  elements[0];
+        elements[0] = elements[SIZE - 1];
+        SIZE--;
+        rearrangeElements(0);
+
+        return priorityElement;
+    }
+
+    public static class Employee implements Comparable<Employee> {
         String name;
         Integer priority;
 
@@ -15,26 +79,28 @@ public class PriorityDS {
             this.name = name;
             this.priority = priority;
         }
-    }
 
-    private static void putElement(Employee arun) {
-        Employee priority = arun;
-
-        if(!highPriorityElements.isEmpty()) {
-            if(arun.priority < highPriorityElements.peek().priority){
-                priority = highPriorityElements.peek();
-            }
+        @Override
+        public int compareTo(Employee o) {
+            return this.priority - o.priority;
         }
-        highPriorityElements.push(priority);
-        elements.push(arun);
-    }
-
-    private static Employee getElement() {
-        return highPriorityElements.pop();
     }
 
 
 
     public static void main(String[] args) {
+        PriorityDS<Employee> ds = new PriorityDS<>(Employee.class);
+
+        putNewElement(ds, new Employee("Arun", 8));
+        getElement(ds);
     }
+
+    private static Employee getElement(PriorityDS<Employee> ds) {
+        return ds.getElement();
+    }
+
+    private static void putNewElement(PriorityDS<Employee> ds, Employee arun) {
+        ds.putElement(arun);
+    }
+
 }
