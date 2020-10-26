a2bd8440daafc5cc7cceadc202ba3ae9e365e755
vivek22117 <vivekmishra22117@gmail.com>
2020-10-26 17:33:35 +0530

    adding ConstantTImeSearch Algo

---
+++ String-Algo/src/main/java/com/ds/algo/examples/searchquest/ConstantTimeSearch.java
@@ -0,0 +1,37 @@
+package com.ds.algo.examples.searchquest;
+
+import java.util.Stack;
+
+public class ConstantTimeSearch {
+
+    private static final Stack<Integer> dataStack = new Stack<>();
+    private static final Stack<Integer> minimumElementStack = new Stack<>();
+
+    public static void main(String[] args) {
+        pushData(5);
+        pushData(11);
+        pushData(7);
+
+        System.out.println(getMinimumElement());
+    }
+
+    private static void pushData(int value) {
+        int min = value;
+        if(!minimumElementStack.isEmpty()) {
+            if(minimumElementStack.peek() < min) {
+                min = minimumElementStack.peek();
+            }
+        }
+        dataStack.push(value);
+        minimumElementStack.push(min);
+    }
+
+    public static Integer getMinimumElement(){
+        return minimumElementStack.peek();
+    }
+
+    public static void popData(){
+        minimumElementStack.pop();
+        dataStack.pop();
+    }
+}
