61ffe09629625e1196e87fdf4544eddea4629148
vivek22117 <vivekmishra22117@gmail.com>
2020-10-27 19:32:06 +0530

    adding MaxSumSubArray Algo

---
+++ String-Algo/src/main/java/com/ds/algo/examples/searchquest/FindMaxSumSubArray.java
@@ -0,0 +1,37 @@
+package com.ds.algo.examples.searchquest;
+
+public class FindMaxSumSubArray {
+
+    private static final int[] inputData = {6, 5, 6 ,-3, 1, 8, -4, 9, 5};
+
+    public static void main(String[] args) {
+
+        int maxSumSubArray = findMaxSumSubArray(inputData);
+        System.out.println("Maximum sum: " + maxSumSubArray);
+    }
+
+    private static int findMaxSumSubArray(int[] inputData) {
+        if(inputData.length == 1) {
+           return inputData[0];
+        }
+
+        int N = inputData.length;
+        int i = 0;
+        int sum = Integer.MIN_VALUE;
+
+        for(int e = 0; e < N; e++) {
+            if(inputData[e] > 0) {
+                int r = e + 1;
+                int interSum = inputData[e];
+                while (r < N){
+                    if(inputData[r] < 0) break;
+                    interSum = interSum + inputData[r];
+                    r++;
+                }
+                sum = Math.max(sum, interSum);
+                e = r;
+            }
+        }
+        return sum;
+    }
+}
