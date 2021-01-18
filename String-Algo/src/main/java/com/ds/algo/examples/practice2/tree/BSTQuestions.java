d20d1e38de1f22319009a9ac7f5a130e247cc713
vivek22117 <vivekmishra22117@gmail.com>
2021-01-18 19:09:08 +0530

    Identify identical BST

--- String-Algo/src/main/java/com/ds/algo/examples/practice2/tree/BSTQuestions.java
+++ String-Algo/src/main/java/com/ds/algo/examples/practice2/tree/BSTQuestions.java
@@ -14,11 +14,11 @@ public class BSTQuestions {
 
 
     public TreeNode<Employee> insert(TreeNode<Employee> head, TreeNode<Employee> newNode) {
-        if(head == null) {
+        if (head == null) {
             return newNode;
         }
 
-        if(newNode.getData().id <= head.getData().id) {
+        if (newNode.getData().id <= head.getData().id) {
             head.setLeftNode(insert(head.getLeftNode(), newNode));
         } else {
             head.setRightNode(insert(head.getRightNode(), newNode));
@@ -28,18 +28,37 @@ public class BSTQuestions {
     }
 
     public TreeNode<Employee> lookUp(TreeNode<Employee> head, TreeNode<Employee> searchNode) {
-        if(head == null) {
+        if (head == null) {
             return null;
         }
 
-        if(head.getData().id == searchNode.getData().id) {
+        if (head.getData().id == searchNode.getData().id) {
             return head;
         }
 
-        if(searchNode.getData().id < head.getData().id) {
+        if (searchNode.getData().id < head.getData().id) {
             return lookUp(head.getLeftNode(), searchNode);
         } else {
             return lookUp(head.getRightNode(), searchNode);
         }
+    }
+
+    public boolean isIdenticalBST(TreeNode<Employee> treeOne, TreeNode<Employee> treeTwo) {
+        if (treeOne == null && treeTwo == null) {
+            return true;
+        }
+
+        if (treeOne == null) {
+            return false;
+        } else if (treeTwo == null) {
+            return false;
+        }
+
+        if(isIdenticalBST(treeOne.getLeftNode(), treeTwo.getLeftNode())
+                && isIdenticalBST(treeOne.getRightNode(), treeTwo.getRightNode())) {
+            return treeOne.getData().id == treeTwo.getData().id;
+        }
+
+        return false;
     }
 }
