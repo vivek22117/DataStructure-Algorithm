9b263893fc1d3d014193881053b9fa9fa5975e27
vivek22117 <vivekmishra22117@gmail.com>
2021-01-26 12:14:14 +0530

    Compress char array algo

---
+++ String-Algo/src/main/java/com/ds/algo/examples/practice2/tree/CompressCharArray.java
@@ -0,0 +1,30 @@
+package com.ds.algo.examples.practice2.tree;
+
+public class CompressCharArray {
+    private static char[] chars = {'a', 'c', 'd', 'd', 'd', 'd', 'i'};
+
+    public static int compress(char[] chars) {
+        int index = 0;
+        int i = 0;
+        while (i < chars.length) {
+            int j = i;
+            while (j < chars.length && chars[j] == chars[i]){
+                j++;
+            }
+            chars[index++] = chars[i];
+            if(j - i > 1) {
+                String count = j - i + "";
+                for (char ch : count.toCharArray()){
+                    chars[index++] = ch;
+                }
+            }
+            i = j;
+        }
+        return index;
+    }
+
+    public static void main(String[] args) {
+        int compress = compress(chars);
+        System.out.println(compress);
+    }
+}
