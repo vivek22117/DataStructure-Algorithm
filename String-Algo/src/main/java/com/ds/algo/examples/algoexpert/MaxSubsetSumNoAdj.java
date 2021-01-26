3a8aab8ff0f48681db1df5e504191a51884cd0c3
vivek22117 <vivekmishra22117@gmail.com>
2021-01-26 19:23:12 +0530

    max sum from subset algo

---
+++ String-Algo/src/main/java/com/ds/algo/examples/algoexpert/MaxSubsetSumNoAdj.java
@@ -0,0 +1,28 @@
+package com.ds.algo.examples.algoexpert;
+
+public class MaxSubsetSumNoAdj {
+    private static int[] data = {23, 47, 8, 41, 11, 33, 55};
+
+    public static int maxSubsetSumNoAdjacent(int[] data) {
+        if(data.length == 0){
+            return -1;
+        } else if(data.length == 1) {
+            return data[0];
+        }
+
+        int secondNum = data[0];
+        int firstNum = Math.max(secondNum, data[1]);
+        for(int i = 2; i < data.length; i++) {
+            int currentNum = Math.max(firstNum, secondNum + data[i]);
+            secondNum = firstNum;
+            firstNum = currentNum;
+        }
+
+        return firstNum;
+    }
+
+    public static void main(String[] args) {
+        int i = maxSubsetSumNoAdjacent(data);
+        System.out.println(i);
+    }
+}
