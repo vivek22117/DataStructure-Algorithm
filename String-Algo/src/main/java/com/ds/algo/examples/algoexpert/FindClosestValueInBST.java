568174ff79db9736fc57c69424d641f675e5d6c0
vivek22117 <vivekmishra22117@gmail.com>
2021-01-26 10:59:19 +0530

    Find closest in BST algo

---
+++ String-Algo/src/main/java/com/ds/algo/examples/algoexpert/FindClosestValueInBST.java
@@ -0,0 +1,42 @@
+package com.ds.algo.examples.algoexpert;
+
+public class FindClosestValueInBST {
+
+    public class TreeNode {
+        int value;
+        TreeNode left;
+        TreeNode right;
+        TreeNode(int x) {
+            value = x;
+        }
+    }
+
+    public int findClosestValue(TreeNode node, int k) {
+
+        int currentClosest = Integer.MAX_VALUE;
+
+        return findClosestValueHelper(node, k, currentClosest);
+    }
+
+    private int findClosestValueHelper(TreeNode node, int target, int currentClosest) {
+        if(node == null) {
+            return currentClosest;
+        }
+
+        if(Math.abs(target - currentClosest) > Math.abs(target - node.value)) {
+            currentClosest = node.value;
+        }
+
+        if(target > node.value) {
+            return findClosestValueHelper(node.right, target, currentClosest);
+        } else if(target < node.value) {
+            return findClosestValueHelper(node.left, target, currentClosest);
+        } else {
+            return currentClosest;
+        }
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
