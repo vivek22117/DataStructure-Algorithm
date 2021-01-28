f4e21ce8cd666b3f58915827f8a1cf9a9b7fb798
vivek22117 <vivekmishra22117@gmail.com>
2021-01-28 09:52:18 +0530

    adding UniqueSubString algo

---
+++ String-Algo/src/main/java/com/ds/algo/examples/practice2/tree/UniqueSubString.java
@@ -0,0 +1,33 @@
+package com.ds.algo.examples.practice2.tree;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class UniqueSubString {
+
+    public static int lengthOfUniqueSubString(String data) {
+        if(data == null || data.length() == 0) {
+            return 0;
+        }
+
+        int i = 0, j = 0, max = 0;
+        Set<Character> set = new HashSet<>();
+        while (i < data.length()) {
+            char ch = data.charAt(i);
+            while (set.contains(ch)){
+                set.remove(data.charAt(j));
+                j++;
+            }
+
+            set.add(ch);
+            max = Math.max(max, i - j+1);
+            i++;
+        }
+        return max;
+    }
+
+    public static void main(String[] args) {
+        int pwwtrsken = UniqueSubString.lengthOfUniqueSubString("PWWTRRSKENN");
+        System.out.println(pwwtrsken);
+    }
+}
