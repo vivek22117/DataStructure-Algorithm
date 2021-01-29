f7bcbe6ee7f7e17d9b0fe1ada0f21edc3500b5bb
vivek22117 <vivekmishra22117@gmail.com>
2021-01-29 18:49:09 +0530

    new algo

--- String-Algo/src/main/java/com/ds/algo/examples/Fibonnaci.java
+++ String-Algo/src/main/java/com/ds/algo/examples/Fibonnaci.java
@@ -1,31 +1,123 @@
-package com.ds.algo.examples;
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.function.*;
+import java.util.regex.*;
+import java.util.stream.*;
+import static java.util.stream.Collectors.joining;
+import static java.util.stream.Collectors.toList;
 
-//Runtime O(n) & Space O(1)
-public class Fibonnaci {
 
-    private int getFibonnaci(int k) {
-        if(k <= 1) {
-            return k;
+
+class Result {
+
+    /*
+     * Complete the 'onesGroups' function below.
+     *
+     * The function is expected to return an INTEGER_ARRAY.
+     * The function accepts following parameters:
+     *  1. 2D_INTEGER_ARRAY grid
+     *  2. INTEGER_ARRAY queries
+     */
+
+    public static List<Integer> onesGroups(List<List<Integer>> grid, List<Integer> queries) {
+
+        Map<Integer, Integer> map = new HashMap<>();
+        int row = grid.size();
+        int col = grid.get(0).size();
+        for(int i =0; i< row; i++) {
+            for(int j = 0; j < col; j++) {
+                int count = 0;
+                if(grid.get(i).get(j) == 1) {
+                    count += dfs(grid, i, j, count);
+                    System.out.println(count);
+                    map.put(count, map.getOrDefault(count, 0) + 1);
+                }
+            }
         }
-
-
-        int first = 1;
-        int second = 0;
-        int result = 0;
-
-        for(int i = 1; i < k; i++) {
-            result = first + second;
-            second = first;
-            first = result;
+        List<Integer> result = new ArrayList<>();
+        for(Integer value : queries){
+            result.add(map.getOrDefault(value, 0));
         }
-
         return result;
     }
 
+    private static int dfs(List<List<Integer>> grid, int row, int col, int count) {
+        if(grid == null || grid.size() == 0) {
+            return 0;
+        }
 
-    public static void main(String[] args) {
-        Fibonnaci fibonnaci = new Fibonnaci();
-        int number = fibonnaci.getFibonnaci(6);
-        System.out.println(number);
+        if(row < 0 || row > grid.size() - 1 ||
+                col < 0 || col > grid.get(row).size() -1 || grid.get(row).get(col) != 1){
+            return 0 ;
+        }
+
+        grid.get(row).set(col, -1);
+
+        if(col > 0 && col < grid.get(row).size() -1) {
+            count += dfs(grid, row, col + 1, count + 1);
+            count += dfs(grid, row, col - 1, count + 1);
+        }
+
+        if(row > 0 && row < grid.size() - 1) {
+            count += dfs(grid, row -1, col, count + 1);
+            count += dfs(grid, row + 1, col, count + 1);
+        }
+
+
+        return count;
+    }
+
+}
+
+class Solution {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        int gridRows = Integer.parseInt(bufferedReader.readLine().trim());
+        int gridColumns = Integer.parseInt(bufferedReader.readLine().trim());
+
+        List<List<Integer>> grid = new ArrayList<>();
+
+        IntStream.range(0, gridRows).forEach(i -> {
+            try {
+                grid.add(
+                        Stream.of(bufferedReader.readLine().replaceAll("\\s+$", "").split(" "))
+                                .map(Integer::parseInt)
+                                .collect(toList())
+                );
+            } catch (IOException ex) {
+                throw new RuntimeException(ex);
+            }
+        });
+
+        int queriesCount = Integer.parseInt(bufferedReader.readLine().trim());
+
+        List<Integer> queries = IntStream.range(0, queriesCount).mapToObj(i -> {
+            try {
+                return bufferedReader.readLine().replaceAll("\\s+$", "");
+            } catch (IOException ex) {
+                throw new RuntimeException(ex);
+            }
+        })
+                .map(String::trim)
+                .map(Integer::parseInt)
+                .collect(toList());
+
+        List<Integer> result = Result.onesGroups(grid, queries);
+
+        bufferedWriter.write(
+                result.stream()
+                        .map(Object::toString)
+                        .collect(joining("\n"))
+                        + "\n"
+        );
+
+        bufferedReader.close();
+        bufferedWriter.close();
     }
 }
