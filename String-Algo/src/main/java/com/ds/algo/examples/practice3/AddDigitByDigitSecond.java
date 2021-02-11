28445049e02ea8c58700f49525a2e390ef0e432a
vivek22117 <vivekmishra22117@gmail.com>
2021-02-11 17:46:23 +0530

    adding digitbydigit algo in practice3

--- String-Algo/src/main/java/com/ds/algo/examples/practice3/AddDigitByDigitSecond.java
+++ String-Algo/src/main/java/com/ds/algo/examples/practice3/AddDigitByDigitSecond.java
@@ -10,7 +10,12 @@ public class AddDigitByDigitSecond {
     private static int[] num2 = {3, 7, 8};
 
     public static void main(String[] args) {
-        addNumDigitByDigit(num1, num2);
+        int[] sum = addNumDigitByDigit(num1, num2);
+
+        Stream.iterate(0, e -> e + 1).limit(sum.length).forEach(e -> {
+            System.out.print(sum[e]);
+        });
+        System.out.println();
     }
 
     private static int[] addNumDigitByDigit(int[] num1, int[] num2) {
@@ -23,6 +28,44 @@ public class AddDigitByDigitSecond {
             return num1;
         }
 
+        int num1LastIndex = num1.length - 1;
+        int num2LastIndex = num2.length - 1;
+
+        int total = 0;
+        int carry = 0;
+        int digit = 0;
+
+        while (num1LastIndex >= 0 && num2LastIndex >= 0) {
+            total = num1[num1LastIndex] + num2[num2LastIndex] + carry;
+            carry = total / 10;
+            digit = total % 10;
+
+            numberSum.add(0, digit);
+            num1LastIndex--;
+            num2LastIndex--;
+        }
+
+        while (num1LastIndex >= 0) {
+            total = num1[num1LastIndex] + carry;
+            carry = total / 10;
+            digit = total % 10;
+
+            numberSum.add(0, digit);
+            num1LastIndex--;
+        }
+
+        while (num2LastIndex >= 0) {
+            total = num2[num2LastIndex] + carry;
+            carry = total / 10;
+            digit = total % 10;
+
+            numberSum.add(0, digit);
+            num2LastIndex--;
+        }
+
+        if(carry != 0) {
+            numberSum.add(0, carry);
+        }
 
         int[] sum = new int[numberSum.size()];
         Stream.iterate(0, e -> e + 1).limit(numberSum.size()).forEach(e -> {
