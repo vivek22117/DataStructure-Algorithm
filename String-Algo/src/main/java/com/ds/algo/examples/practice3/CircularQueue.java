f7255dead8c877d3a9d689be37f6a22949b69007
vivek22117 <vivekmishra22117@gmail.com>
2021-02-11 18:20:51 +0530

    Practice second circularQueue DS

---
+++ String-Algo/src/main/java/com/ds/algo/examples/practice3/CircularQueue.java
@@ -0,0 +1,25 @@
+package com.ds.algo.examples.practice3;
+
+import java.lang.reflect.Array;
+
+public class CircularQueue<T> {
+
+    private static final int MAX_SIZE = 55;
+    private static final int SPECIAL_EMPTY_VALUE = -1;
+
+    private T[] element;
+
+    private int tailIndex = SPECIAL_EMPTY_VALUE;
+    private int headIndex = SPECIAL_EMPTY_VALUE;
+
+    public CircularQueue(Class<T> clazz) {
+        element = (T[])Array.newInstance(clazz, MAX_SIZE);
+    }
+
+    public CircularQueue(Class<T> clazz, int size) {
+        element = (T[])Array.newInstance(clazz, size);
+    }
+
+
+
+}
