f57c832a74b9bcb716bfa9cd7bdea52f1c0dfa9a
vivek22117 <vivekmishra22117@gmail.com>
2021-02-17 16:10:52 +0530

    new algo merge BST

---
+++ String-Algo/src/main/java/com/ds/algo/examples/practice3/MergeTwoBST.java
@@ -0,0 +1,30 @@
+package com.ds.algo.examples.practice3;
+
+public class MergeTwoBST {
+
+    class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode(int x) {
+            val = x;
+        }
+    }
+
+    public TreeNode mergeBST(TreeNode t1, TreeNode t2) {
+        if(t1 == null) {
+            return t2;
+        }
+
+        if(t2 == null) {
+            return t1;
+        }
+
+        t1.val += t2.val;
+        t1.left = mergeBST(t1.left, t2.left);
+        t1.right = mergeBST(t1.right, t2.right);
+
+        return t1;
+    }
+}
