a323d5982c239612c9f3a956aaa4144a8fff34c4
vivek22117 <vivekmishra22117@gmail.com>
2021-02-17 16:45:29 +0530

    BST path sum algo

---
+++ String-Algo/src/main/java/com/ds/algo/examples/practice3/GroupedAnagram.java
@@ -0,0 +1,32 @@
+package com.ds.algo.examples.practice3;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+
+public class GroupedAnagram {
+
+    private static String[] data = {"ate", "eat", "nat", "tan", "tea"};
+
+    public static void main(String[] args) {
+        groupAnagrams(data);
+    }
+
+    private static void groupAnagrams(String[] data) {
+        List<List<String>> groupedAnagram = new ArrayList<>();
+        HashMap<String, List<String>> map = new HashMap<>();
+
+        for(String val : data) {
+            char[] chars = val.toCharArray();
+            Arrays.sort(chars);
+            String sorted = new String(chars);
+
+            if(!map.containsKey(sorted)){
+                map.put(sorted, new ArrayList<>());
+            }
+
+            map.get(sorted).add(val);
+        }
+    }
+}
