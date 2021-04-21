77f65f4dfcd033a728ce57726b91fda6a7f67c1a
vivek22117 <vivekmishra22117@gmail.com>
2021-04-21 10:33:33 +0530

    BST path sum algo

--- String-Algo/src/main/java/test/TotalSampleVenkat.java
+++ String-Algo/src/main/java/test/TotalSampleVenkat.java
@@ -11,6 +11,8 @@ public class TotalSampleVenkat {
         List<Integer> numbers = Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8, 9, 10);
 
         System.out.println(totalValue(numbers, e -> true));
+        System.out.println(totalValue(numbers, e -> e % 2 == 0));
+        System.out.println(totalValue(numbers, e -> e % 2 != 0));
     }
 
     private static int totalValue(List<Integer> numbers, Predicate<Integer> selector) {
@@ -18,9 +20,8 @@ public class TotalSampleVenkat {
         int total = 0;
 
         for(int e : numbers) {
-            total += e;
+            if(selector.test(e)) total += e;
         }
         return total;
-
     }
 }
