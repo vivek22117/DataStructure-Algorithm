cd3ddbeeac5c1298e63401475b97381a520f88a6
vivek22117 <vivekmishra22117@gmail.com>
2021-10-19 22:33:23 +0530

    BST path sum algo

---
+++ String-Algo/src/main/java/com/ds/algo/examples/algoacademy/MaxSumSubArray.java
@@ -0,0 +1,32 @@
+package com.ds.algo.examples.algoacademy;
+
+
+/*
+Input: nums = [-2, -5, 6, -2, -3, 1, 5, -6]
+Output: 7
+Explanation: sum([6, -2, -3, 1, 5]) = 7
+ */
+public class MaxSumSubArray {
+
+    private static int[] input = {-2, -5, 6, 2, -3, 1, 6, -6};
+
+    public static void main(String[] args) {
+        MaxSumSubArray max = new MaxSumSubArray();
+        int result = max.maxSum(input);
+        System.out.println(result);
+    }
+
+
+    public int maxSum(int[] nums) {
+        int sum = nums[0];
+        for(int i = 0; i < nums.length; i++) {
+            int tempSum = 0;
+            for(int j = i; j < nums.length; j++) {
+                tempSum += nums[j];
+                sum = Math.max(sum, tempSum);
+            }
+        }
+
+        return sum;
+    }
+}
