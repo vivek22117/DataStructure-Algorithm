0841da5dfc1c26ec6770facae790aed4eca47b5f
vivek22117 <vivekmishra22117@gmail.com>
2021-10-21 08:05:28 +0530

    BST path sum algo

--- String-Algo/src/main/java/com/ds/algo/examples/algoacademy/NumOfPathsToReachLastCell.java
+++ String-Algo/src/main/java/com/ds/algo/examples/algoacademy/NumOfPathsToReachLastCell.java
@@ -2,4 +2,40 @@ package com.ds.algo.examples.algoacademy;
 
 public class NumOfPathsToReachLastCell {
 
+    private static int[][] input = {{4, 7, 1, 6}, {5, 7, 3, 9}, {3, 2, 1, 2}, {7, 1, 6, 3}};
+
+    public static void main(String[] args) {
+        NumOfPathsToReachLastCell paths = new NumOfPathsToReachLastCell();
+        int result = paths.numOfPaths(input, 25);
+        System.out.println(result);
+    }
+
+    public int numOfPaths(int[][] grid, int target) {
+        if(target < 0) {
+            return -1;
+        }
+
+        return findPaths(grid, 0, 0, target);
+    }
+
+    private int findPaths(int[][] grid, int row, int col, int target) {
+        if(row == grid.length -1 && col == grid[0].length -1) {
+            return (grid[row][col] - target == 0) ? 1 : 0;
+        }
+
+        if(row == grid.length -1) {
+            return findPaths(grid, grid.length -1, col + 1, target);
+        }
+
+        if(col == grid[0].length - 1) {
+            return findPaths(grid, row - 1, grid[row].length -1, target);
+        }
+
+        int rowPath = findPaths(grid, row + 1, col, target);
+        int colPath = findPaths(grid, row, col + 1, target);
+
+        return rowPath + colPath;
+
+    }
+
 }
