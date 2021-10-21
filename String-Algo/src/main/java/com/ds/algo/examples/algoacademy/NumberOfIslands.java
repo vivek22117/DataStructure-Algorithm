170ffc3e1f7188b50ed55c5eaeafc3a757577b2b
vivek22117 <vivekmishra22117@gmail.com>
2021-10-21 11:01:19 +0530

    BST path sum algo

---
+++ String-Algo/src/main/java/com/ds/algo/examples/algoacademy/NumberOfIslands.java
@@ -0,0 +1,57 @@
+package com.ds.algo.examples.algoacademy;
+
+import java.util.ArrayList;
+import java.util.List;
+
+
+/*
+Input:
+1 1 1 1 0
+1 1 0 1 0
+1 1 0 0 0
+0 0 0 0 1
+
+Output: [9]
+Explanation:  There is only one island with size 9
+ */
+
+public class NumberOfIslands {
+
+    private static int[][] input = {{1, 1, 1, 1, 0}, {1, 1, 0, 1, 0}, {1, 1, 0, 0, 0}, {0, 0, 0, 1}};
+
+    public static void main(String[] args) {
+        NumberOfIslands islands = new NumberOfIslands();
+        List<Integer> result = islands.sizeOfIslands(input);
+        System.out.println(result);
+    }
+
+    public List<Integer> sizeOfIslands(int[][] grid) {
+        List<Integer> result = new ArrayList<>();
+
+        for (int i = 0; i < grid.length; i++) {
+            for (int j = 0; j < grid[i].length; j++) {
+
+                if (grid[i][j] == 1) {
+                    result.add(dfs(grid, i, j));
+                }
+            }
+        }
+
+        return result;
+    }
+
+    private int dfs(int[][] grid, int row, int col) {
+        if (row >= grid.length || row < 0 || col >= grid[row].length || col < 0 || grid[row][col] == 0) {
+            return 0;
+        }
+
+        grid[row][col] = 0;
+        int counter = 1;
+        counter += dfs(grid, row + 1, col);
+        counter += dfs(grid, row - 1, col);
+        counter += dfs(grid, row, col + 1);
+        counter += dfs(grid, row, col - 1);
+
+        return counter;
+    }
+}
