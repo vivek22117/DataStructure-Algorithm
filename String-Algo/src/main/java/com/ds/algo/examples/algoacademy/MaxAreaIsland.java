fedf84ff7ff28a6b48609c1479271548fbd70d21
vivek22117 <vivekmishra22117@gmail.com>
2021-10-21 11:49:07 +0530

    BST path sum algo

---
+++ String-Algo/src/main/java/com/ds/algo/examples/algoacademy/MaxAreaIsland.java
@@ -0,0 +1,59 @@
+package com.ds.algo.examples.algoacademy;
+
+
+/*
+Input:
+1 1 0 1 0
+1 1 0 1 0
+1 1 0 0 0
+0 0 0 0 1
+
+Output: 6
+Explanation:  There are 3 island with size 6, 2, 1
+ */
+public class MaxAreaIsland {
+
+    private static int[][] input = {
+            {1, 1, 0, 1, 1},
+            {1, 1, 0, 1, 1},
+            {1, 1, 0, 0, 1},
+            {1, 1, 0, 1, 1}
+    };
+
+    public static void main(String[] args) {
+        MaxAreaIsland size = new MaxAreaIsland();
+        int result = size.findMaxArea(input);
+        System.out.println(result);
+    }
+
+    public int findMaxArea(int[][] grid) {
+        int maxResult = 0;
+        for(int i = 0; i < grid.length; i++) {
+            for(int j = 0; j < grid[i].length; j++) {
+                if(grid[i][j] == 1) {
+                    int currentSize = dfs(grid, i, j);
+                    System.out.println(currentSize);
+                    maxResult = Math.max(maxResult, currentSize);
+                }
+            }
+        }
+
+        return maxResult;
+    }
+
+    private int dfs(int[][] grid, int row, int col) {
+        if(row >= grid.length || row < 0 || col >= grid[row].length || col < 0 || grid[row][col] == 0) {
+         return 0;
+        }
+
+        grid[row][col] = 0;
+        int size = 1;
+        size += dfs(grid, row, col + 1);
+        size += dfs(grid, row, col -1);
+        size += dfs(grid, row - 1, col);
+        size += dfs(grid, row + 1, col);
+
+        return size;
+    }
+
+}
