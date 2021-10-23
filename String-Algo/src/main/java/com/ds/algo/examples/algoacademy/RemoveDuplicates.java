51505b68e9d388e3b8dae43363ace7d330c99513
vivek22117 <vivekmishra22117@gmail.com>
2021-10-23 20:19:52 +0530

    BST path sum algo

--- String-Algo/src/main/java/com/ds/algo/examples/algoacademy/RemoveDuplicates.java
+++ String-Algo/src/main/java/com/ds/algo/examples/algoacademy/RemoveDuplicates.java
@@ -3,6 +3,7 @@ package com.ds.algo.examples.algoacademy;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.HashSet;
 import java.util.List;
 
 /*
@@ -20,6 +21,9 @@ public class RemoveDuplicates {
 
         int[] result2 = duplicates.processMoreEfficient(input);
         System.out.println(Arrays.toString(result2));
+
+        int[] result3 = duplicates.processMostEfficiently(input);
+        System.out.println(Arrays.toString(result3));
     }
 
 //  Big O(n2)
@@ -63,5 +67,23 @@ public class RemoveDuplicates {
             }
         }
         return Arrays.copyOfRange(nums, 0, index + 1);
+    }
+
+//  Big O(n) & Space Big O (n)
+    public int[] processMostEfficiently(int[] nums) {
+        if(nums.length == 0) {
+            return new int[]{};
+        }
+
+        if(nums.length == 1) {
+            return nums;
+        }
+
+        HashSet<Integer> uniqueValues = new HashSet<>();
+        for(int value : nums) {
+            uniqueValues.add(value);
+        }
+
+        return uniqueValues.stream().mapToInt(i -> i).toArray();
     }
 }
