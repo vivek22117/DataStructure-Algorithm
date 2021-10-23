c7c6aeb2c19ef31ae403e0336a387d83c7908a09
vivek22117 <vivekmishra22117@gmail.com>
2021-10-23 20:06:42 +0530

    BST path sum algo

---
+++ String-Algo/src/main/java/com/ds/algo/examples/algoacademy/RemoveDuplicates.java
@@ -0,0 +1,67 @@
+package com.ds.algo.examples.algoacademy;
+
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+/*
+Input: [2, 3, 1, 1, 4, 3, -2, 1]
+Output: [2, 3, 1, 4, -2]
+ */
+public class RemoveDuplicates {
+
+    private static int[] input = {2, 3, 1, 1, 4, 3, -2, 1};
+
+    public static void main(String[] args) {
+        RemoveDuplicates duplicates = new RemoveDuplicates();
+        int[] result = duplicates.process(input);
+        System.out.println(Arrays.toString(result));
+
+        int[] result2 = duplicates.processMoreEfficient(input);
+        System.out.println(Arrays.toString(result2));
+    }
+
+//  Big O(n2)
+    public int[] process(int[] nums) {
+        if(nums.length == 0) {
+            return new int[]{};
+        }
+
+        if(nums.length == 1) {
+            return nums;
+        }
+
+        List<Integer> result = new ArrayList<>();
+        for(int value : nums) {
+            if(!result.contains(value)) {
+                result.add(value);
+            }
+        }
+
+        return result.stream().mapToInt(i -> i).toArray();
+    }
+
+//  Big O(nlog n)
+    public int[] processMoreEfficient(int[] nums) {
+        if(nums.length == 0) {
+            return new int[]{};
+        }
+
+        if(nums.length == 1) {
+            return nums;
+        }
+
+        Arrays.sort(nums);
+
+        int index = 0;
+        for(int i = 1; i < nums.length; i++) {
+            if(nums[i] != nums[i - 1]){
+                System.out.println(index);
+                nums[++index] = nums[i];
+                System.out.println(index);
+            }
+        }
+        return Arrays.copyOfRange(nums, 0, index + 1);
+    }
+}
