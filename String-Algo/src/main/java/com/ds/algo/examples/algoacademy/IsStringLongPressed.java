59ea35ac4a49d825ce937a436fb35ff3bf983789
vivek22117 <vivekmishra22117@gmail.com>
2021-10-24 10:31:26 +0530

    new algo Is String long pressed

--- String-Algo/src/main/java/com/ds/algo/examples/algoacademy/IsStringLongPressed.java
+++ String-Algo/src/main/java/com/ds/algo/examples/algoacademy/IsStringLongPressed.java
@@ -11,41 +11,44 @@ Output: True
  */
 public class IsStringLongPressed {
 
-    private static String input = "vivek";
-    private static String longPressedInput = "viiiveeeeekkk";
+    private static String input = "vivekk";
+    private static String longPressedInput = "viiivvekkkk";
 
     public static void main(String[] args) {
+        IsStringLongPressed longPressed = new IsStringLongPressed();
+        boolean result = longPressed.isLongPressedInput(input, longPressedInput);
 
+        System.out.println(result);
     }
 
 
     public boolean isLongPressedInput(String input, String longPressedInput) {
-        int m = input.length();
-        int n = longPressedInput.length();
+        int n = input.length();                                                 // 2
+        int m = longPressedInput.length();                                      // 4
 
-        if(n < m) {
+        if(m < n) {
             return false;
         }
 
         int i = 0;
         int j = 0;
-        while(i < n && j < m) {
-            char ch1 = input.charAt(i);
-            char ch2 = input.charAt(j);
+        while(i < n && j < m) {                                                 // i < 2 && j < 4
+            char ch1 = input.charAt(i);                                         // v i
+            char ch2 = longPressedInput.charAt(j);                              // v i
 
             if(ch1 != ch2) {
                 return false;
             }
 
-            int a = i + 1;
-            int b = j + 1;
+            int a = i + 1;                                                      // 1   2
+            int b = j + 1;                                                      // 1   2  3
 
-            while (a < n && input.charAt(a) == ch1) {
-                ++a;
+            while (a <= input.length() -1  && input.charAt(a) == ch1) {
+                a++;
             }
 
-            while (b < m && longPressedInput.charAt(b) == ch2) {
-               ++b;
+            while (b <= longPressedInput.length() - 1 && longPressedInput.charAt(b) == ch2) {
+               b++;
             }
 
             i = a;
