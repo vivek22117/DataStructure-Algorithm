b34431de18ee0f5c914fb12548c5831127ba6acf
vivek22117 <vivekmishra22117@gmail.com>
2021-10-25 22:05:53 +0530

    BST path sum algo

---
+++ String-Algo/src/main/java/com/ds/algo/examples/algoacademy/LongestConsecutiveSequence.java
@@ -0,0 +1,11 @@
+package com.ds.algo.examples.algoacademy;
+
+
+/*
+Input: [100, 4, 200, 1, 3, 2]
+Output: 4
+Explanation: Longest consecutive sequence is [1, 2, 3, 4].
+             Therefore its length is 4.
+ */
+public class LongestConsecutiveSequence {
+}
