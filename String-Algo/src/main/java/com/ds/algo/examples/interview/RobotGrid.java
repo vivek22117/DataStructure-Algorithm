1135ba7c87afd93734dc6cfd051638738f78d4ec
vivek22117 <vivekmishra22117@gmail.com>
2021-10-27 22:32:59 +0530

    BST path sum algo

---
+++ String-Algo/src/main/java/com/ds/algo/examples/interview/RobotGrid.java
@@ -0,0 +1,24 @@
+package com.ds.algo.examples.interview;
+
+
+/*
+Problem: We have an m x n grid. A robot is placed at the top-left corner of this grid.
+The robot can only move either right or down at any point in time, but it is not allowed
+to move in certain cells. The robot's goal is to find a path from the top-left corner
+to the bottom-right corner of the grid.
+
+Not allowed cells have value True
+ */
+
+public class RobotGrid {
+
+    private static boolean[][] grid = {{}, {}, {}, {}};
+
+    public static void main(String[] args) {
+
+    }
+
+    public boolean computePath(boolean[][] grid) {
+
+    }
+}
