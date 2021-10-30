14c4ec762af902cf9427b88db726bc00777cdfe4
vivek22117 <vivekmishra22117@gmail.com>
2021-10-30 18:45:18 +0530

    BST path sum algo

--- String-Algo/src/main/java/com/ds/algo/examples/algoacademy/SubArrayTargetSum.java
+++ String-Algo/src/main/java/com/ds/algo/examples/algoacademy/SubArrayTargetSum.java
@@ -11,10 +11,13 @@ public class SubArrayTargetSum {
     private static int[] input = {10, 3, 5, 8, 6, 12, 20, 15, 31};
 
     public static void main(String[] args) {
-
+        SubArrayTargetSum sum = new SubArrayTargetSum();
+        String result = sum.getSubArray(input, 31);
+        System.out.println(result);
     }
 
     public String getSubArray(int[] nums, int target) {
+        String result = "";
         int N = nums.length;
 
         if(N == 0) {
@@ -26,14 +29,18 @@ public class SubArrayTargetSum {
         int endIndex = 1;
 
         while (endIndex < N) {
-            if(sum == target) {
-                return startIndex + " " + (endIndex - 1);
-            }
 
-            if(sum < target && endIndex < N) {
+            if(sum < target) {
                 sum += nums[endIndex];
                 endIndex++;
+            } else if(sum > target && startIndex < endIndex) {
+                sum -= nums[startIndex];
+                startIndex++;
+            } else {
+                result = startIndex + " " + (endIndex - 1);
+                break;
             }
         }
+        return result;
     }
 }
