b8ae269e734ba86c4872c6c35a594ebea5f2191a
vivek22117 <vivekmishra22117@gmail.com>
2021-11-10 20:52:51 +0530

    BST path sum algo

--- String-Algo/src/main/java/com/ds/algo/examples/algoacademy/MinimumPathSum.java
+++ String-Algo/src/main/java/com/ds/algo/examples/algoacademy/MinimumPathSum.java
@@ -67,4 +67,25 @@ public class MinimumPathSum {
                 Math.min(findMin(grid, cache, i + 1, j), findMin(grid, cache, i, j + 1));
         return cache[i][j];
     }
+
+    public int getMinPathSum(int[][] grid) {
+        if(grid.length == 0) {
+            return -1;
+        }
+
+        for(int i = 0; i < grid.length; i++) {
+            for(int j = 0; j < grid[i].length; j++) {
+                int top = i - 1 < 0 ? Integer.MAX_VALUE : grid[i - 1][j];
+                int left = j - 1 < 0 ? Integer.MAX_VALUE : grid[i][j - 1];
+
+                if(top == Integer.MAX_VALUE && left == Integer.MAX_VALUE) {
+                    grid[i][j] = 0;
+                } else {
+                    grid[i][j] = Math.min(top, left);
+                }
+            }
+        }
+        return grid[grid.length - 1][grid[0].length - 1];
+
+    }
 }
