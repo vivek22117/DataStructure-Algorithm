b846acffcd155dcd8fce4c2535b8306ad54b73ab
vivek22117 <vivekmishra22117@gmail.com>
2021-11-10 16:01:52 +0530

    adding new algo

---
+++ String-Algo/src/main/java/test/ParseLogsFetchTopK.java
@@ -0,0 +1,47 @@
+package test;
+
+import java.io.*;
+import java.util.*;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.function.Function;
+import java.util.function.ToLongFunction;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+public class ParseLogsFetchTopK {
+
+    private static String logPath = "C:\\projects\\Development\\DataStructure-Algorithm\\data";
+
+    public static void main(String[] args) throws IOException {
+        ParseLogsFetchTopK parser = new ParseLogsFetchTopK();
+        parser.readAndParseLogs(logPath);
+    }
+
+    public void readAndParseLogs(String logPath) throws IOException {
+        Path path = Paths.get(logPath, "apache_log");
+
+        Stream<String> lines = Files.lines(path);
+        Map<String, Long> map = lines.map(value -> value.split(" ")[0])
+                .collect(Collectors.groupingBy(Function.identity(), Collectors.counting()));
+
+        List<Map.Entry<String, Long>> sortedList = new ArrayList<>();
+
+//        List<Map.Entry<String, Long>> sortedList = new ArrayList<>(map.entrySet());
+//        sortedList.addAll(map.entrySet());
+
+        for(Map.Entry<String, Long> val : map.entrySet()) {
+            sortedList.add(val);
+        }
+
+        Collections.sort(sortedList, Comparator.comparingLong(new ToLongFunction<Map.Entry<String, Long>>() {
+            @Override
+            public long applyAsLong(Map.Entry<String, Long> entry) {
+                return entry.getValue();
+            }
+        }));
+
+        System.out.println(map);
+    }
+}
