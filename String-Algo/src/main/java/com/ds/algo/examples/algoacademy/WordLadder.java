98c959d4e0b7137728ae1824e8334c8ef8bd93ae
vivek22117 <vivekmishra22117@gmail.com>
2021-11-11 21:31:23 +0530

    BST path sum algo

--- String-Algo/src/main/java/com/ds/algo/examples/algoacademy/WordLadder.java
+++ String-Algo/src/main/java/com/ds/algo/examples/algoacademy/WordLadder.java
@@ -1,6 +1,11 @@
 package com.ds.algo.examples.algoacademy;
 
 
+import java.util.ArrayDeque;
+import java.util.Deque;
+import java.util.HashMap;
+import java.util.Map;
+
 /*
 Given two words (beginWord and endWord), and a dictionary's word list, find the length of shortest transformation sequence from beginWord to endWord, such that:
 
@@ -36,7 +41,41 @@ public class WordLadder {
     }
 
     public int ladderLength(String[] words, String beginWord, String endWord) {
+        Deque<String> queue = new ArrayDeque<>();
+        Map<String, Integer> minDistMap = new HashMap<>();
 
+        queue.add(beginWord);
+        minDistMap.put(beginWord, 1);
+
+        while (!queue.isEmpty()) {
+            String word = queue.poll();
+
+            if(endWord.equals(word)){
+                break;
+            }
+
+            for(String nextWord : words) {
+                if(numOfChanges(word, nextWord) != 1) {
+                    continue;
+                }
+                if(minDistMap.containsKey(nextWord)) {
+                    continue;
+                }
+                minDistMap.put(nextWord, minDistMap.get(word) + 1);
+                queue.add(nextWord);
+            }
+        }
+        return minDistMap.getOrDefault(endWord, 0);
+    }
+
+    private int numOfChanges(String word, String nextWord) {
+        int diff = 0;
+
+        for(int i = 0; i < word.length(); i++) {
+            diff += (word.charAt(i) != nextWord.charAt(i)) ? 1 : 0;
+        }
+
+        return diff;
     }
 
 }
