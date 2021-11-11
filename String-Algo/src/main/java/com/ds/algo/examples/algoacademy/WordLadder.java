4acef046feaa43249dbca53d40deb1c37db44385
vivek22117 <vivekmishra22117@gmail.com>
2021-11-11 20:31:22 +0530

    BST path sum algo

---
+++ String-Algo/src/main/java/com/ds/algo/examples/algoacademy/WordLadder.java
@@ -0,0 +1,42 @@
+package com.ds.algo.examples.algoacademy;
+
+
+/*
+Given two words (beginWord and endWord), and a dictionary's word list, find the length of shortest transformation sequence from beginWord to endWord, such that:
+
+Only one letter can be changed at a time.
+Each transformed word must exist in the word list.
+===========================================================
+Input:
+beginWord = "hit",
+endWord = "cog",
+wordList = ["hot","dot","dog","lot","log","cog"]
+
+Output: 5
+
+Explanation: As one shortest transformation is "hit" -> "hot" -> "dot" -> "dog" -> "cog",
+return its length 5.
+
+==============================================================
+Input:
+beginWord = "hit"
+endWord = "cog"
+wordList = ["hot","dot","dog","lot","log"]
+
+Output: 0
+
+Explanation: The endWord "cog" is not in wordList, therefore no possible transformation.
+ */
+public class WordLadder {
+
+    private static String[] wordList = {"hot","dot","dog","lot","log","cog"};
+
+    public static void main(String[] args) {
+
+    }
+
+    public int ladderLength(String[] words, String beginWord, String endWord) {
+
+    }
+
+}
