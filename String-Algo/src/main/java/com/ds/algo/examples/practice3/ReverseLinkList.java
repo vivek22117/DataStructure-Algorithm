120b50dfc272033c3b71f83d13798219b5914bce
vivek22117 <vivekmishra22117@gmail.com>
2021-11-14 19:27:13 +0530

    BST path sum algo

--- String-Algo/src/main/java/com/ds/algo/examples/practice3/ReverseLinkList.java
+++ String-Algo/src/main/java/com/ds/algo/examples/practice3/ReverseLinkList.java
@@ -1,10 +1,47 @@
 package com.ds.algo.examples.practice3;
 
+import com.ds.algo.examples.practice1.linklist.LinkListDS;
+
+import java.util.List;
+
 public class ReverseLinkList {
 
-    class ListNode {
+    private static ListNode head = null;
+
+    public static void main(String[] args) {
+        ReverseLinkList list = new ReverseLinkList();
+        ListNode node = new ListNode(19);
+        ListNode node2 = new ListNode(1);
+        ListNode node3 = new ListNode(11);
+        ListNode node4 = new ListNode(18);
+        ListNode node5 = new ListNode(22);
+        ListNode node6 = new ListNode(23);
+
+
+
+
+        head = node;
+        head.next = node2;
+        node2.next = node3;
+        node3.next = node4;
+        node4.next = node5;
+        node5.next = node6;
+
+        list.printLinkList();
+
+        ListNode reverse = list.reverse(head);
+        head = reverse;
+        list.printLinkList();
+    }
+
+    static class ListNode {
         int val;
         ListNode next;
+
+        ListNode(int x, ListNode node) {
+            val = x;
+            next = node;
+        }
 
         ListNode(int x) {
             val = x;
@@ -23,5 +60,15 @@ public class ReverseLinkList {
         }
 
         return prev;
+    }
+
+    public void printLinkList() {
+        ListNode current = head;
+        while (current != null){
+            System.out.print(current.val + "->");
+            current = current.next;
+        }
+        System.out.println("Null");
+        System.out.println("Printing Done...!!");
     }
 }
