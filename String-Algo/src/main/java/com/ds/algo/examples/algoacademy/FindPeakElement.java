003c5d661544ebc5d3e8622165d87ffe38a569c2
vivek22117 <vivekmishra22117@gmail.com>
2022-12-12 10:15:27 +0530

    BST path sum algo

--- String-Algo/src/main/java/com/ds/algo/examples/algoacademy/FindPeakElement.java
+++ String-Algo/src/main/java/com/ds/algo/examples/algoacademy/FindPeakElement.java
@@ -20,8 +20,36 @@ public class FindPeakElement {
         System.out.println(peakElement);
 
         System.out.println(result);
+
+        int peakElementFromArray = getPeakElementFromArray(input);
+        System.out.println(peakElementFromArray);
+
     }
 
+    private static int getPeakElementFromArray(int[] nums) {
+        if( nums.length == 0) {
+            return -1;
+        }
+
+        for(int i = 0; i < nums.length; i++) {
+            if(currentElementIsPeak(nums, i)) {
+                return i;
+            }
+        }
+        return 0;
+    }
+
+    private static boolean currentElementIsPeak(int[] nums, int index) {
+        if(index == 0) {
+            return nums[index] > nums[index + 1];
+        }
+
+        if(index == nums.length -1) {
+            return nums[index] > nums[index -1];
+        }
+
+        return nums[index -1] < nums[index] && nums[index] > nums[index + 1];
+    }
 
     public int getPeak(int[] nums) {
         if(nums.length == 0) {
